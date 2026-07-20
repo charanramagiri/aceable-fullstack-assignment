407d24a343c976a432d60cf455b8904ab9211d96
charan <charanramagiri2004@gmail.com>
2026-07-20 23:39:20 +0530

    removed not useful code

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from .api.health_views import health_check
 from .api.upload_views import upload_files
 from .api.search_views import search_test
-from .views import parse_test
 
 urlpatterns = [
     path("health/", health_check, name="health-check"),
     path("upload/", upload_files, name="upload-files"),
-    path("parse-test/", parse_test),
     path("search/", search_test),
-    
 ]
